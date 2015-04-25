b4e5d0b0ac9eee347188768e3317326f5c59362b
Karl Hiramoto <karl@hiramoto.org>
2015-04-25 18:11:30 +0200

    use PID file to get PID

--- recipes-xoware/xoscripts/files/xokd-watcher.sh
+++ recipes-xoware/xoscripts/files/xokd-watcher.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
+PID_FILE='/tmp/xokd.pid'
+
 #deamonize so we close stdout, if you want to see the debug you can remove '-d'
-xokd -d
+xokd -P ${PID_FILE} -d
 sleep 3
 
-XOKD_PID=`ps |grep xokd |grep -v S80 |grep -v watcher|grep -v grep |awk '{ print $1}'|head -n1`
+#XOKD_PID=`ps |grep xokd |grep -v S80 |grep -v watcher|grep -v grep |awk '{ print $1}'|head -n1`
 
+if [ -e ${PID_FILE} ]
+then 
+	XOKD_PID=`cat ${PID_FILE}`
+else
+	logger "xokd PID FILE ${PID_FILE} not found" 
+	sleep 20
+	reboot
+	exit
+fi
 
 # stay here while xokd running
 while [ -e /proc/${XOKD_PID} ]; do
