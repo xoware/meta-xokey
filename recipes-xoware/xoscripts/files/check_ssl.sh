110bc4753702d730fac0dd6ea7d44f1f1e18ffe9
Karl Hiramoto <karl@hiramoto.org>
2015-06-12 21:01:49 +0200

    validate certificates

--- recipes-xoware/xoscripts/files/check_ssl.sh
+++ recipes-xoware/xoscripts/files/check_ssl.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-if  [ -e /xokcfg/server.key ]; then
-#	echo "key exists"
-	exit 
+generate_certs () {
+	echo "GENERATE CERTS"
+	cd /xokcfg
+	rm -f  server.key server.crt
+	openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout server.key -out server.crt -subj '/C=US/ST=California/L=Sunnyvale/CN=xo1.xoware.com'
+}
+
+
+openssl rsa -in  /xokcfg/server.key  -noout
+if [ "$?" -ne 0 ]
+then
+	logger -s "KEY invalid.. regenerate"
+	generate_certs
 fi
 
-cd /xokcfg
 
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout server.key -out server.crt -subj '/C=US/ST=California/L=Sunnyvale/CN=xo1.xoware.com'
+openssl x509 -in /xokcfg/server.crt  -noout
+if [ "$?" -ne 0 ]
+then
+	logger -s "cert bad.. regenerate"
+	generate_certs
+fi
